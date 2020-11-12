5c8ce6d27ccd9442c12992c997d1fea2ab3b28e4
Arturo Cordoba <arturocv16@gmail.com>
2020-11-11 22:49:34 -0600

    Servidor secuencial

--- server.c
+++ server.c
@@ -8,13 +8,17 @@
 #include <arpa/inet.h>
 
 #include "receive_image.c"
+#include "general_functions.c"
 
-void listenClient(int serverSocket);
+char* folderpath;
+
+void attendRequest(int clientSocket, int id);
+void createFolder();
 
 int main() {
     // Creacion de directorios
-    //char* dfolderpath = createDataFolders(); // Guardar ruta del folder de almacenamiento
-    //printf("Ruta: %s\n\n", dfolderpath);
+    createFolder();
+    printf("%s\n", folderpath);
 
     // Creacion del descriptor del socket
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -31,51 +35,91 @@ int main() {
     // Escucha de conexiones entrantes
     listen(serverSocket, __INT_MAX__);
 
+    int processCount = 0;
+    // Manejo de las consultas de los clientes
     while (1) {
-        listenClient(serverSocket);
+        // Estructura para obtener la informacion del cliente
+        struct sockaddr_in clientAddr;
+        unsigned int sin_size = sizeof(clientAddr);
+
+        // Se espera por una conexion con un cliente
+        int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, &sin_size);
+
+        // Procesamiento de la solicitud
+        processCount++;
+        attendRequest(clientSocket, processCount);
+        printf("%d solicitudes recibidas!\n", processCount);
     }
 
     // Cerrar la conexion
     shutdown(serverSocket, SHUT_RDWR);
-    //free(dfolderpath);
+    
+    free(folderpath);
     return 0;
 }
 
-void listenClient(int serverSocket) {
-    // Estructura para obtener la informacion del cliente
-    struct sockaddr_in clientAddr;
-    unsigned int sin_size = sizeof(clientAddr);
-
-    // Se espera por una conexion con un cliente
-    int clientSocket = accept(serverSocket, (struct sockaddr *) &clientAddr, &sin_size);
-    char *ipClient = inet_ntoa(clientAddr.sin_addr);
-    printf("Conexion entrante: %s\n", ipClient);
-
+/**
+ * Funcion que se encarga de atender la solicitud del cliente
+ * clientSocket: identificador del socket del cliente
+ * id: identificador del proceso que atiende la solicutd
+*/
+void attendRequest(int clientSocket, int id) {
     unsigned char* buffer = (char*) malloc(sizeof(unsigned char)*BUFFER_SIZE);
-    while (1) {
-        // Limpieza del buffer
-        memset(buffer, 0, sizeof(unsigned char)*BUFFER_SIZE);
-        
-        // Se espera por el mensaje de inicio
-        if (recv(clientSocket, buffer, BUFFER_SIZE, 0) == 0) { 
-            // Se perdio la conexion con el cliente
-            break;
-        }
+    
+    // Limpieza del buffer
+    memset(buffer, 0, sizeof(unsigned char)*BUFFER_SIZE);
+    
+    // Se espera por el mensaje de inicio
+    recv(clientSocket, buffer, BUFFER_SIZE, 0);
 
-        // Recepcion del archivo
-        Image* image = receiveImage(clientSocket);
+    // Recepcion del archivo
+    Image* image = receiveImage(clientSocket);
 
-        Image filtered = sobel_filter(*image);
-        writeImage("filtrada.png", filtered);
-        
-        free(image->data);
-        free(filtered.data);
-        free(image);
+    // Procesamiento de la imagen
+    Image filtered = sobel_filter(*image);
+
+    // Guardado de la imagen
+    if (id <= 100) {
+        char* s_id = int2str(id);
+        char* filename = concat(s_id,".png");
+        char* filepath = concat(folderpath, filename);
+        writeImage(filepath, filtered);
+
+        free(s_id);
+        free(filename);
+        free(filepath);
     }
-
-    printf("Conexion perdida! Esperando nueva conexion...\n");
+    
+    // Limpieza de memoria
+    free(image->data);
+    free(filtered.data);
+    free(image);
     free(buffer);
 
     // Se cierra la conexion
     shutdown(clientSocket, SHUT_RDWR);
+}
+
+/**
+ * Funcion para crear las carpetas del servidor
+*/
+void createFolder() {
+    // Creacion carpeta raiz de los servidor Heavy Process
+    createDirectory("sequential");
+
+    char *sCounter = NULL;
+    char *name = NULL;
+    int created = -1; 
+    int counter = 0;
+
+    // Se determina el numero de contenedor correspondiente
+    while (created != 0){
+        counter++;
+        sCounter = int2str(counter);
+        name = concat("sequential/server", sCounter);
+        created = createDirectory(name);
+    }
+    // Se actualiza la variable global
+    folderpath = concat(name, "/");
+    free(name);
 }
