9d201de0180a1479555dc49db426013ce47f42ad
Arturo Cordoba <arturocv16@gmail.com>
2020-11-11 21:37:34 -0600

    Creacion de hilos en el cliente

--- client.c
+++ client.c
@@ -2,30 +2,40 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include <pthread.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+#include "general_functions.c"
 #include "read_image.c"
 #include "constants.c"
+
+typedef struct Request {
+    int ncycles; 
+    int port; 
+    char* serverIP;
+    Image image;
+} Request;
 
 int isPNG(char* filename);
 int sendImage(Image image, int socket);
 char* int2str(int number);
 char* concat(const char *s1, const char *s2);
+void* sendRequest(void* request);
 
 int main(int argc, char **argv) {
-    if(argc != 4) {
-        printf("Debe ingresar ip, puerto, imagen como argumentos\n");
+    if(argc != 6) {
+        printf("Debe ingresar ip, puerto, imagen, threads y ciclos como argumentos\n");
         return 0;
     }
     // Direccion IP del servidor
     char* serverIP = argv[1];
 
     // Puerto del servidor
-    char* serverPort = argv[2];
-    int port = atoi(serverPort);
+    int port = atoi(argv[2]);
 
     // Ruta de la imagen
     char* filepath = argv[3];
@@ -34,49 +44,90 @@ int main(int argc, char **argv) {
         return -1;
     }
 
+    // Numero de threads
+    int nthreads = atoi(argv[4]);
+
+    // Numero de ciclos
+    int ncycles = atoi(argv[5]);
+
     // Lectura de la imagen
     Image image = readImage(filepath); 
 
-    // Creacion del socket
-    int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    
-    // Configuracion de direccion y puerto del cliente
-    struct sockaddr_in serverAddr;
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port); // Puerto
-    serverAddr.sin_addr.s_addr = inet_addr(serverIP); // Direccion IP del servidor 
+    // Creacion del struct para pasar argumentos a los hilos
+    Request* request = malloc(sizeof(request));
+    request->ncycles = ncycles;
+    request->port = port;
+    request->serverIP = serverIP;
+    request->image = image;
 
-    // Se intenta conectar con el puerto de la direccion ip establecida
-    int connectionStatus = connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-    
-    // Verificar estado de conexion
-    if (connectionStatus == -1) {
-        printf("Error al intentar conectar con el servidor\n");
-        return -1;
+    // Creacion de los hilos
+    pthread_t threads_id[nthreads];
+    for (int i = 0; i < nthreads; i++) {
+        pthread_t thread;
+        pthread_create(&threads_id[i], NULL, sendRequest, request);
     }
 
-    unsigned char* buffer = (char*) malloc(sizeof(unsigned char)*BUFFER_SIZE);
-    while (1) {
-        // Limpieza del buffer
-        memset(buffer, 0, sizeof(unsigned char)*BUFFER_SIZE);
+    printf("Todos los hilos creados, esperando finalizacion\n");
 
-        // Envio del mensaje de inicio
-        send(clientSocket, START_MSG, BUFFER_SIZE, 0);
-
-        // Envio del archivo
-        sendImage(image, clientSocket);
+    // Espera a que todos los hilos finalicen
+    for (int i = 0; i < nthreads; i++)
+       pthread_join(threads_id[i], NULL);
         
-        // Envio mensaje de finalizacion
-        if (!send(clientSocket, END_MSG, sizeof(END_MSG), 0)) {
-            printf("Error al enviar mensaje de finalizacion");
-            break;
-        }
-        break;  // BORRAR
-    }
-    // Se cierra la conexion con el servidor
-    shutdown(clientSocket, SHUT_RDWR);
     return 0;
+}
 
+/**
+ * Funcion para enviar una cantidad n de solicitud al servidor
+ * requestStruct: struct con los argumentos de la solicitud
+*/
+void* sendRequest(void* requestStruct){
+    // Parametros de la solicitud
+    Request* request = (Request*) requestStruct;
+    int ncycles = request->ncycles;
+    int port = request->port;
+    char* serverIP = request->serverIP;
+    Image image = request->image;
+
+    for (int i = 0; i < ncycles; i++) {
+        // Creacion del socket
+        int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        
+        // Configuracion de direccion y puerto del cliente
+        struct sockaddr_in serverAddr;
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(port); // Puerto
+        serverAddr.sin_addr.s_addr = inet_addr(serverIP); // Direccion IP del servidor 
+
+        // Se intenta conectar con el puerto de la direccion ip establecida
+        int connectionStatus = connect(clientSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+        
+        // Verificar estado de conexion
+        if (connectionStatus == -1) {
+            printf("Error al intentar conectar con el servidor\n");
+            exit(1);
+        }
+
+        unsigned char* buffer = (char*) malloc(sizeof(unsigned char)*BUFFER_SIZE);
+        while (1) {
+            // Limpieza del buffer
+            memset(buffer, 0, sizeof(unsigned char)*BUFFER_SIZE);
+
+            // Envio del mensaje de inicio
+            send(clientSocket, START_MSG, BUFFER_SIZE, 0);
+
+            // Envio del archivo
+            sendImage(image, clientSocket);
+            
+            // Envio mensaje de finalizacion
+            if (!send(clientSocket, END_MSG, sizeof(END_MSG), 0)) {
+                printf("Error al enviar mensaje de finalizacion");
+                break;
+            }
+            break;  // BORRAR
+        }
+        // Se cierra la conexion con el servidor
+        shutdown(clientSocket, SHUT_RDWR);
+    }
 }
 
 /**
@@ -153,7 +204,6 @@ int sendImage(Image image, int socket) {
     return 0;
 }
 
-
 /**
  * Funcion para verificar que un archivo tiene terminacion .png
  * filename: string con el nombre del archivo
@@ -176,31 +226,4 @@ int isPNG(char* filename) {
     }
     free(copy);
     return 0;
-}
-
-/**
- * Funcion para obtener un string a partir de un numero
- * number: numero que se desea convertir a string
- * return: string que representa al numero
-*/
-char* int2str(int number) {
-    int n = snprintf(NULL, 0, "%d", number);
-    char* sNumber = malloc(n+1);
-    snprintf(sNumber, n+1, "%d", number);
-    return sNumber;
-}
-
-/**
- * Funcion para concatenar un string s2 al final de un string s1
- * s1: string que se desea concatenar
- * s2: string que se desea concatenar
- * return: string con la concatenacion de s1 y s2
-*/ 
-char* concat(const char *s1, const char *s2) {
-    const size_t len1 = strlen(s1); // Largo de s1
-    const size_t len2 = strlen(s2); // Largo de s2
-    char *result = malloc(len1 + len2 + 1); // +1 para el null-terminator
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, len2 + 1);
-    return result;
 }
