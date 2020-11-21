510174e14cbf80c3348d1e4140e3338d969437de
Arturo Cordoba <arturocv16@gmail.com>
2020-11-21 17:22:55 -0600

    Grafica Cantidad de solicitudes vs Tiempo promedio por solicitud

--- visualizer.m
+++ visualizer.m
@@ -38,7 +38,40 @@ hold on;
 plot(hp_req,hp_time,'-ro', 'Color', 'magenta');
 plot(php_req,php_time,'-ro', 'Color', 'blue');
 xlabel("Cantidad de solicitudes");
-ylabel("Tiempo [s]");
+ylabel("Tiempo en completar todas[s]");
 title('Cantidad de solicitudes vs Tiempo en completar todas');
 legend('Sequential', 'Heavy Process', 'Pre Heavy Process');
 hold off;
+
+% Calculo del promedio segun cantidad de solicitudes
+seq_prom = [];
+for i = 1:n1/2
+  seq_prom = [seq_prom seq_time(i)/seq_req(i)];
+end  
+
+hp_prom = [];
+for i = 1:n2/2
+  hp_prom = [hp_prom hp_time(i)/hp_req(i)];
+end  
+
+php_prom = [];
+for i = 1:n3/2
+  php_prom = [php_prom php_time(i)/php_req(i)];
+end  
+
+% Grafica Cantidad de solicitudes vs Tiempo promedio por solicitud
+figure;
+plot(seq_req,seq_prom,'-ro', 'Color', 'red');
+hold on;
+plot(hp_req,hp_prom,'-ro', 'Color', 'magenta');
+plot(php_req,php_prom,'-ro', 'Color', 'blue');
+xlabel("Cantidad de solicitudes");
+ylabel("Tiempo promedio por solicitud [s]");
+title('Cantidad de solicitudes vs Tiempo promedio por solicitud');
+legend('Sequential', 'Heavy Process', 'Pre Heavy Process');
+hold off;
+
+  
+  
+  
+  
